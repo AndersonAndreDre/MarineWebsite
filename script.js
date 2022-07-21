3a38564e8e728ea64ef5d2f5a5b657893542a93a
Anderson Moyo <74625898+AndersonAndreDre@users.noreply.github.com>
2022-07-21 13:02:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function readFun() {
+   document.getElementById("readmode").style.backgroundColor="black";
+}
+
+function SignUpFun() {
+  document.getElementById("signUp").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
